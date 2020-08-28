089307de25fe4e5e1110a8b93a3d2fed8c1db0da
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-27 23:25:29 -0300

    Add exercise 6

---
+++ Fundamentals/exercise6.py
@@ -0,0 +1,4 @@
+firstNumber = int(input('Write the first number: '))
+sucessor = firstNumber + 1
+predecessor = firstNumber - 1
+print('The number {} has the predecessor {} and sucessor {}'.format(firstNumber, predecessor, sucessor))
