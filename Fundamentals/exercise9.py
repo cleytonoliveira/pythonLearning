07872534dc01c790498c401cadc4ed0a37b7fede
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-28 00:05:09 -0300

    Add exercise 9

---
+++ Fundamentals/exercise9.py
@@ -0,0 +1,4 @@
+meterNumber = int(input('Write a number in meter: '))
+convertToCentimeter = meterNumber * 100
+convertToMillimeter = meterNumber * 1000
+print('Convert {} meter to centimeter is {} and to millimete is {}'.format(meterNumber, convertToCentimeter, convertToMillimeter))
