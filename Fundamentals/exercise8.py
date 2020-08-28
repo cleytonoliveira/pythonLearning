c95d54bbeb489973ca9ece27f220a55003a6ca33
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-27 23:55:26 -0300

    Add exercise 8

---
+++ Fundamentals/exercise8.py
@@ -0,0 +1,4 @@
+firstGrade = int(input('Write the first note: '))
+secondGrade = int(input('Write the second note: '))
+average = (firstGrade + secondGrade) / 2
+print('The first grade of student was {}, second grade was {} and your average was {}'.format(firstGrade, secondGrade, average))
