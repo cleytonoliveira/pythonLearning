78bffa3edfe0762b28a4b1be5cdc67ba9894845b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-28 00:47:46 -0300

    Add exercise 14

---
+++ Fundamentals/exercise14.py
@@ -0,0 +1,3 @@
+salary = int(input('How much is your salary? '))
+calculateImprove = salary + (salary * 0.15)
+print('Your salary is R${}, but you received 15% of higher and now your salary is R${}'.format(salary, calculateImprove))
