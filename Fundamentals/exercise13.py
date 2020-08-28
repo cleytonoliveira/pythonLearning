c6786b925c4edb3d0800abe0e9684ad4d46fbe10
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-28 00:41:08 -0300

    Add exercise 13

---
+++ Fundamentals/exercise13.py
@@ -0,0 +1,3 @@
+price = int(input('What the price now? '))
+discount = price - (price * 0.05)
+print('The original price is R${} and with the discount of 5% the price is R${}'.format(price, discount))
