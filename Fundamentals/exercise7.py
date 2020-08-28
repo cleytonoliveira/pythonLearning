b2e509ab742b24ac7dfea856311b83e8e3d269cf
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-27 23:44:34 -0300

    Add exercise 7

---
+++ Fundamentals/exercise7.py
@@ -0,0 +1,8 @@
+import math
+
+firstNumber = int(input('Write a number: '))
+double = firstNumber * 2
+triple = firstNumber * 3
+square = math.sqrt(firstNumber)
+
+print('The number {} has a {} with double, {} with triple and {} with square root'.format(firstNumber, double, triple, square))
